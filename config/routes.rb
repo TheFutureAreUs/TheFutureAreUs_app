54bc5457b909b7328d4a2c710b3439bd60f1fdff
Kwesi Larbi <kwesiiilarb@gmail.com>
2017-06-11 18:01:32 -0400

    Adding pages for terms, contact, etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :contacts, only: [:new, :create]
 
   root 'categories#index'
+
+  match '/about', to: 'pages#about', via: :get
+  match '/privacy', to: 'pages#privacy', via: :get
+  match '/terms', to: 'pages#terms', via: :get
+  # match '/contact', to: 'pages#contact', via: :get
 end
