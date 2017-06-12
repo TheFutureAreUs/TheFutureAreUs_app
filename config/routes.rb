a1bcc5ee8a5b3c27032eea2be77d5f36dd984608
Kwesi Larbi <kwesiiilarb@gmail.com>
2017-06-12 00:10:40 -0400

    Switching methods, We want to give the Users the ability to create subcategories

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root 'categories#index'
 
+
   match '/about', to: 'pages#about', via: :get
   match '/privacy', to: 'pages#privacy', via: :get
   match '/terms', to: 'pages#terms', via: :get
