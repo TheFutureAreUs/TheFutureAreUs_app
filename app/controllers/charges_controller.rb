60ab34efd06c49e84c4f4f8a10c3288490fafce9
Kwesi Larbi <kwesiiilarb@gmail.com>
2017-07-02 15:01:03 -0400

    combined submit and payment button, need to figure out routes later

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -13,10 +13,15 @@ class ChargesController < ApplicationController
     charge = StripeTool.create_charge(customer_id: customer.id,
                                       amount: @amount,
                                       description: @description)
+    
+    redirect_to thanks_path
 
   rescue Stripe::CardError => e 
     flash[:error] = e.message
     redirect_to new_charge_path
+  end
+
+  def thanks
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :contacts, only: [:new, :create]
   resources :categories
   resources :charges, only: [:new, :create]
+  get 'thanks', to: 'charges#thanks', as: 'thanks'
   
 
   # Adding search functionality
