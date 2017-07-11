618e91e74fd0c7c81b86b057274a51c1f9de1ea4
Kwesi Larbi <kwesiiilarb@gmail.com>
2017-07-11 10:20:07 -0400

    Made listings specific to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,7 +25,7 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @listings = Listing.all.order("created_at DESC").paginate(page: params[:page], per_page: 6)
+    @listings = Listing.where(category_id: params[:id]).order("created_at DESC").paginate(page: params[:page], per_page: 6)
     @category = Category.find(params[:id])
   end
 
